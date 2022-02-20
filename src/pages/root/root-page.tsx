79665d790a1a7d7b10ffe057fca42c5cb67d42da
Audrius Butkus <audrius.butkus@telesoftas.com>
2022-02-20 10:28:23 +0200

    chore: added initial personalities

--- src/pages/root/root-page.tsx
+++ src/pages/root/root-page.tsx
@@ -1,5 +1,51 @@
 import React from 'react';
 
+const personalities: Personality[] = [
+  { type: 'ENTJ' },
+  { type: 'ESTJ' },
+  { type: 'ENFJ' },
+  { type: 'ESFJ' },
+  { type: 'ENTP' },
+  { type: 'ESTP' },
+  { type: 'ENFP' },
+  { type: 'ESFP' },
+  { type: 'INTJ' },
+  { type: 'ISTJ' },
+  { type: 'INFJ' },
+  { type: 'ISFJ' },
+  { type: 'INTP' },
+  { type: 'ISTP' },
+  { type: 'INFP' },
+  { type: 'ISFP' },
+];
+
+interface Personality {
+  type: string;
+}
+
+const extraverts: Personality[] = personalities.filter((p) =>
+  p.type.startsWith('E')
+);
+const introverts: Personality[] = personalities.filter((p) =>
+  p.type.startsWith('I')
+);
+
+const Introverts = (): JSX.Element => {
+  const introvertTable = introverts.map((p) => <td>{p.type}</td>);
+  return (
+    <>
+      <table style={{ width: '100%' }}>
+        <tr>{introvertTable}</tr>
+      </table>
+    </>
+  );
+};
+
 export const RootPage = () => {
-  return <div>Welcome to personality</div>;
+  return (
+    <div>
+      <h4>Introverts:</h4>
+      <Introverts />
+    </div>
+  );
 };
