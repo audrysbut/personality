cac742aab7d5f082d96c816de8ddc4b4ce83a714
Audrius Butkus <audrius.butkus@telesoftas.com>
2022-02-20 10:07:37 +0200

    chore: removed unrequired routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
-function One() {
-  return <div>Home</div>;
-}
-
-function Two() {
-  return <div>Path</div>;
-}
+import { RootPage } from './pages/root/root-page';
 
 function App() {
   return (
     <Router basename="/">
       <div>
-        <div>
-          <div>Pages:</div>
-          <div>
-            <Link to="/">Home</Link>
-          </div>
-          <div>
-            <Link to="/path">Path</Link>
-          </div>
-        </div>
         <Switch>
           <Route exact path="/">
-            <One />
-          </Route>
-          <Route path="/path">
-            <Two />
+            <RootPage />
           </Route>
         </Switch>
       </div>
