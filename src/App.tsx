fa00c099196cb647ba96e78fb2405a82c62e9a3b
Audrius Butkus <audrius.butkus@telesoftas.com>
2022-02-20 10:31:57 +0200

    chore: removed unused link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { RootPage } from './pages/root/root-page';
 
 function App() {
